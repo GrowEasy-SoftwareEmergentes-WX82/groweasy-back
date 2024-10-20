70461dfb647fc7611d19e632c4b5b2d2dbb61179
wfuturi35 <wilfredofuturi1234567@gmail.com>
2024-10-20 18:38:59 -0500

    First commit

---
+++ src/main/java/wx82/agrotech/groweasyapi/GroweasyApiApplication.java
@@ -0,0 +1,13 @@
+package wx82.agrotech.groweasyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroweasyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroweasyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wx82/agrotech/groweasyapi/GroweasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wx82.agrotech.groweasyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroweasyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
